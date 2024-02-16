4d5071377dce7807bd261ad92357e2e323160db8
Laurie-anne Doré <lau.adore@gmail.com>
2024-02-16 10:53:03 -0400

    design navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
+import * as React from "react";
 import "./navbar.css";
 import logo from "../../assets/logo/logo.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav>
-            <img src={logo} alt="Logo de Kasa" className="logo" />
+            <img src={logo} alt="Logo de Kasa" />
             <ul>
                 <li>
-                    Accueil
+                    <Link to="/" >Accueil</Link>
                 </li>
                 <li>
-                    A Propos
+                    <Link to="/about">A propos</Link>
                 </li>
             </ul>
         </nav>
