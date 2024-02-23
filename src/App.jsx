f949bb2baf56dbd4ea8696ce8f5bef4cc1a6e207
Laurie-anne Doré <lau.adore@gmail.com>
2024-02-23 13:50:56 -0400

    Correction design
    Responsive :  Correction breakpoints, modification cards vTablettes.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Error from './pages/Error/Error';
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </BrowserRouter>
