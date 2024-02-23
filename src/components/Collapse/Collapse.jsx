f6622998af0a81e8b357a4160b0a35c16aa5604c
Laurie-anne Doré <lau.adore@gmail.com>
2024-02-23 08:51:04 -0400

    Animations collapse v1

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import './collapse.css';
-import arrowDown from '../../assets/icons/arrowDown.svg';
 import arrowUp from '../../assets/icons/arrowUp.svg';
 
 const Collapse = ({ title, content }) => {
     const [isOpen, setIsOpen] = useState(false);
 
+    const toggleState = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-            <li className='collapse' onClick={() => setIsOpen(!isOpen)}>
-                <div>
-                    <h3>{title}</h3>
-                    <img src={isOpen ? arrowDown : arrowUp} alt={title} />
-                </div>
-                {isOpen && <p>{content}</p>}
-            </li>
+        <li className={`collapse ${isOpen ? 'open' : ''}`} onClick={toggleState}>
+            <div>
+                <h3>{title}</h3>
+                <img src={arrowUp} alt={title} className={isOpen ? 'rotate' : ''} />
+            </div>
+            {isOpen && <p>{content}</p>}
+        </li>
     );
 };
 
